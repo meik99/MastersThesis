d2f4703468f7d2067b0ba9eb29c1dc545aef5500
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-10-05 13:48:37 +0200

    testing custom workflow

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -eu
+
+ls -lah
+
 mkdir -p build
 mkdir -p output
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./build MAIN.tex
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./build MAIN.tex
+ls -lah
 pushd build || exit 10
 biber --input-directory=../ MAIN
 popd || exit 20
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./build MAIN.tex
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./build MAIN.tex
 
 mv build/main.pdf output/thesis.pdf
 
