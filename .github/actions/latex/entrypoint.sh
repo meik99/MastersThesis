ac49f0284405c661ab51da393bd3bce292e8caad
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-12-12 16:50:03 +0100

    changed actions

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -9,6 +9,8 @@ mkdir -p output
 pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
 biber MAIN
 pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
+biber MAIN
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
 mv MAIN.pdf output/thesis.pdf
 
 
