c424977100ce16b4841396288f6a9e1df5450e52
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-12-12 16:54:16 +0100

    more compilation for github actions

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -11,6 +11,7 @@ biber MAIN
 pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
 biber MAIN
 pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
+pdflatex -halt-on-error -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf MAIN.tex
 mv MAIN.pdf output/thesis.pdf
 
 
